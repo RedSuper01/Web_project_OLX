e00d6034d5c353309a08b17515b9b8d7281d0f17
RedSuper <anastasia.kim.mobile@gmail.com>
2022-03-30 13:17:06 +0600

    Cделана страница About me(будет еще дополняться)

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def main():
     return render_template("main.html")
 
+@app.route('/about_me')
+def about_me():
+    return render_template("about_me.html")
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
